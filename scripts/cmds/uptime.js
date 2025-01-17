438f8e6073670073dd551edc6cffcb64496312b0
Lawkeycylx2 <lawkeymarvellous@gmail.com>
2025-01-17 16:00:49 +0100

    Update uptime.js

--- scripts/cmds/uptime.js
+++ scripts/cmds/uptime.js
@@ -23,6 +23,6 @@ module.exports = {
     const hours = Math.floor((uptime / (60 * 60)) % 24);
     const days = Math.floor(uptime / (60 * 60 * 24));
     const uptimeString = `${hours} hours ${minutes} minutes ${seconds} second`;
-    api.sendMessage(`hello user, the bot has been running for ${uptimeString}.`, event.threadID);
+    api.sendMessage(`hello Master Lawkey Marvellous, I, 𝕃𝕒𝕨𝕜𝕖𝕪 𝕔𝕒𝕝𝕪𝕩 has been running for ${uptimeString}.`, event.threadID);
   }
 };
